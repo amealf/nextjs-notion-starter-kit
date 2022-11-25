4b1ed31bd63826b657cfe9224da9706c67695b5b
amealf <amealf@163.com>
2022-11-25 17:10:56 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -14,12 +14,12 @@ export default siteConfig({
   author: 'ylm',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Chinese articles translated by tuzhi',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'amealf',
+  github: '',
+  linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
